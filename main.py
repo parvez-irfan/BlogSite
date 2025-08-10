57e10d876833a9ebb5cf6726fcbbd425dcae132c
Irfan Parvez <irfanparvez@Irfans-MacBook-Air.local>
2025-08-10 18:13:27 +0600

    VENV variable fixes

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ import datetime as dt
 from functools import wraps
 import bleach
 from bleach.css_sanitizer import CSSSanitizer
+import os 
+from dotenv import load_dotenv 
+load_dotenv()
 
 ALLOWED_TAGS = [
     # Text formatting
@@ -88,7 +91,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor= CKEditor(app)
 app.config['CKEDITOR_PKG_TYPE'] = 'basic'
 
@@ -97,7 +100,7 @@ app.config['CKEDITOR_PKG_TYPE'] = 'basic'
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog_website.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI','sqlite:///blog_website.db') # the second name is the fallback
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -393,5 +396,6 @@ def logout():
     return redirect(url_for('login'))
 
 
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    app.run(port=5003)
